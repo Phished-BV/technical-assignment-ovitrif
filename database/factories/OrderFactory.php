69bef4b631ddf397e5afd4f65d09abcedc154305
Ovi Trif <octrif@gmail.com>
2023-12-07 19:53:22 +0100

    Add factory for order model

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        // generate a random integer
+        return [
+            'total' => rand(1, 5000),
+        ];
+    }
+}

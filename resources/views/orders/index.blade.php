95fa7040af2e49c450ac3cc4ddb1126c9eb7b708
Ovi Trif <octrif@gmail.com>
2023-12-08 18:40:49 +0100

    Add button to reply

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -7,17 +7,23 @@
                     <tr class="text-left">
                         <th class="p-2">Id</th>
                         <th class="p-2">Total</th>
-                        <th class="p-2">Address</th>
+                        <th class="p-2 w-2/6">Address</th>
                         <th class="p-2">Recipient</th>
+                        <th class="p-2"></th>
                     </tr>
                     </thead>
                     <tbody>
                     @foreach ($orders as $order)
-                        <tr class="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-gray-100">
+                        <tr class="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-gray-100 group">
                             <td class="p-2">{{ $order->public_id }}</td>
                             <td class="p-2">${{ $order->total }}</td>
                             <td class="p-2">{{ $order->address }}</td>
                             <td class="p-2">{{ $order->recipient }}</td>
+                            <td class="p-2 text-right">
+                                <x-secondary-button class="invisible group-hover:visible">
+                                    {{ __('Reply') }}
+                                </x-secondary-button>
+                            </td>
                         </tr>
                     @endforeach
                     </tbody>

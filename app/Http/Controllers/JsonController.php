e57185a52a2cb3fe42d791e86f81674928ac6d40
Ovi Trif <octrif@gmail.com>
2023-12-08 13:25:19 +0100

    Add logic to extract order total

--- app/Http/Controllers/JsonController.php
+++ app/Http/Controllers/JsonController.php
@@ -13,10 +13,31 @@ class JsonController extends Controller
         $jsonData = $request->json()->all();
 
         // Process the JSON data
-        $jsonString = json_encode($jsonData);
+        $messageBody = $jsonData['Snippet'];
+        $jsonString = $this->parseEmail($messageBody);
+        // $jsonString = $this->parseSummary($jsonData);
+        // $keys = array_keys($jsonData);
+        // $keysString = implode(', ', $keys);
         Log::info($jsonString);
 
         // Return a JSON response if needed
         return response()->json(['message' => 'Data received and processed']);
+    }
+
+    // TODO return an associative array of the order data
+    private function parseEmail($emailBody): string
+    {
+        $total = $this->extractTotal($emailBody);
+        return "Total Value: $total";
+    }
+
+    private function extractTotal($messageBody): int
+    {
+        $pattern = '/Total:\s+\$(\d+);/';
+        if (preg_match($pattern, $messageBody,$matches)) {
+            return (int) $matches[1];
+        } else {
+            return 0;
+        }
     }
 }

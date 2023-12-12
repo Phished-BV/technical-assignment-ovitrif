53d35165658fa0100f20ebfc2ba370a2efaa381c
Ovi Trif <octrif@gmail.com>
2023-12-12 16:39:57 +0100

    Remove commented code in OrderFactory

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -18,10 +18,6 @@ class OrderFactory extends Factory
     public function definition(): array
     {
         return [
-            // Fill with fake data
-            // 'total' => $this->faker->numberBetween(100, 5000),
-            // 'address' => $this->faker->address(),
-            // 'recipient' => $this->faker->name(),
         ];
     }
 }
